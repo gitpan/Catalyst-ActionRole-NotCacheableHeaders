4d913d3c97a8e7102b1f59b440657728082f3c73
Alex J. G. Burzyński <ajgb@cpan.org>
2010-09-19 20:22:42 +0000

    Import of AJGB/Catalyst-ActionRole-NotCacheableHeaders-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-ActionRole-NotCacheableHeaders
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AJGB/Catalyst-ActionRole-NotCacheableHeaders-0.02.tar.gz
    gitpan-cpan-author:       AJGB
    gitpan-cpan-maturity:     released

--- lib/Catalyst/ActionRole/NotCachableHeaders.pm
+++ lib/Catalyst/ActionRole/NotCachableHeaders.pm
@@ -1,77 +1,35 @@
-package Catalyst::ActionRole::NotCachableHeaders;
+package # hide from CPAN
+    Catalyst::ActionRole::NotCachableHeaders;
 BEGIN {
-  $Catalyst::ActionRole::NotCachableHeaders::VERSION = '0.01';
+  $Catalyst::ActionRole::NotCachableHeaders::VERSION = '0.02';
 }
-# ABSTRACT: Set no cache headers for actions
 
 use strict;
 use Moose::Role;
-use HTTP::Date qw(time2str);
 
-after 'execute' => sub {
-    my $self = shift;
-    my ($controller, $c, @args) = @_;
-
-    if ( exists $c->action->attributes->{NotCachable} ) {
-        $c->response->header(
-            'Expires' =>  'Thu, 01 Jan 1970 00:00:00 GMT',
-            'Pragma' => 'no-cache',
-            'Cache-Control' => 'no-cache',
-            'Last-Modified' => time2str( time ),
-        );
-    }
-};
+sub BUILD {
+    die "please use Catalyst::ActionRole::NotCacheableHeaders instead"
+       ." (typo in package name)";
+}
 
 no Moose::Role;
-
 1; # End of Catalyst::ActionRole::NotCachableHeaders
 
 
 
-__END__
 =pod
 
 =encoding utf-8
 
 =head1 NAME
 
-Catalyst::ActionRole::NotCachableHeaders - Set no cache headers for actions
+Catalyst::ActionRole::NotCachableHeaders
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
-=head1 SYNOPSIS
-
-    package MyApp::Controller::Foo;
-    use Moose;
-    use namespace::autoclean;
-
-    BEGIN { extends 'Catalyst::Controller::ActionRole' }
-
-    __PACKAGE__->config(
-        action_roles => [qw( NotCachableHeaders )],
-    );
-
-    sub dont_cache_me : Local NotCachable { ... }
-
-=head1 DESCRIPTION
-
-Provides a ActionRole to set HTTP headers instructing proxies and browsers to
-not cache the response.
-
-    Pragma: no-cache
-    Cache-Control: no-cache
-    Expires: Thu, 01 Jan 1970 00:00:00 GMT
-    Last-Modified: %current time%
-
-Please note that if any of above headers were already set they will be
-overwritten.
-
-=head1 SEE ALSO
-
-Take a look at L<Catalyst::ActionRole::ExpiresHeader> if you want to set the
-C<Expires> header only.
+=for Pod::Coverage   BUILD
 
 =head1 AUTHOR
 
@@ -85,4 +43,9 @@ This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
+
+
+__END__
+
+
 
